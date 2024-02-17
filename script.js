2f5f7f0ea05fd9a952e64ad2f6dd77bf2b7f73df
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-16 16:51:48 -0800

    Create column and row function for given gridsize

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const grid = document.querySelector(".container");
 const buttons = document.querySelectorAll("button")
+const holdColumn = document.createElement("div");
+holdColumn.className = "grid-column";
+const holdRow = document.createElement("div");
+holdRow.className = "grid-row";
 
-let gridSize;
+let gridSize = 16;
 
-getGridSize();
+createRow(gridSize);
+createColumn(gridSize);
 
-function getGridSize() {
-    buttons.forEach(button  => {
-        button.addEventListener("click", () => {
-            gridSize = button.className;
-            createGrid(gridSize);
-        });
-    });
-};
+function  createRow(gridSize) {
+    for (let i = 0; i < (gridSize); i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        holdRow.appendChild(row);
+        grid.appendChild(holdRow);
+     }
+}
 
-function  createGrid(gridSize) {
-    for (let row = 0; row  < gridSize; row++) {
-        for (let column = 0; column < gridSize; column++) {
-        const div = document.createElement("div");
-        div.className = "grid-item";
-        grid.appendChild(div);
-        }
+function createColumn(gridSize) {
+    for (let j = 0; j < gridSize - 1; j++) {
+        const column = document.createElement("div");
+        column.className = "column";
+        holdColumn.appendChild(column);
+        grid.appendChild(holdColumn);
     }
 }
+
+//function getGridSize() {
+//    buttons.forEach(button  => {
+//        button.addEventListener("click", () => {
+//            gridSize = button.className;
+//            createGrid(gridSize);
+//        });
+//    });
+//};
