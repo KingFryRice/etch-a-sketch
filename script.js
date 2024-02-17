c5f53c6f08c25519ad02f69dafc65c40a01ccd39
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-16 21:51:14 -0800

    Create event listener on buttons to adjust grid size

--- script.js
+++ script.js
@@ -4,14 +4,31 @@ const buttons = document.querySelectorAll("button")
 let gridSize = 16;
 
 createGrid(gridSize);
+updateGrid();
 
 function  createGrid(gridSize) {
     for (let i = 0; i < (gridSize * gridSize); i++) {
         const cell = document.createElement("div");
-        cell.className = "row";
+        cell.className = "cell";
         grid.appendChild(cell);
         cell.addEventListener ("mouseover", () => {
-            cell.style = "background: black; transition: .1s;";
+            cell.style = "background: rgb(32, 31, 31); transition: .1s;";
         });
      };
+};
+
+function updateGrid () {
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            let newGridSize = button.className;
+            for (let i = 0; i < (newGridSize * newGridSize); i++) {
+                const newCell = document.createElement("div");
+                newCell.className = "new-cell";
+                grid.replaceChild(cell, newCell);
+                newCell.addEventListener ("mouseover", () => {
+                    newCell.style = "background: rgb(32, 31, 31); transition: .1s;"; 
+                });
+            };
+        });
+    });
 };
