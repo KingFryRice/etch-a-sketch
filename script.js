df1a7d357a31004ef60628e88572c6c010bcf117
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-16 20:24:17 -0800

    Rebuild grid builder to creat 16x16 grid of divs in container

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
 const grid = document.querySelector(".container");
 const buttons = document.querySelectorAll("button")
-const holdColumn = document.createElement("div");
-holdColumn.className = "grid-column";
-const holdRow = document.createElement("div");
-holdRow.className = "grid-row";
 
 let gridSize = 16;
 
 createRow(gridSize);
-createColumn(gridSize);
 
 function  createRow(gridSize) {
-    for (let i = 0; i < (gridSize); i++) {
+    for (let i = 0; i < (gridSize * gridSize); i++) {
         const row = document.createElement("div");
         row.className = "row";
-        holdRow.appendChild(row);
-        grid.appendChild(holdRow);
+        grid.appendChild(row);
      }
 }
-
-function createColumn(gridSize) {
-    for (let j = 0; j < gridSize - 1; j++) {
-        const column = document.createElement("div");
-        column.className = "column";
-        holdColumn.appendChild(column);
-        grid.appendChild(holdColumn);
-    }
-}
-
-//function getGridSize() {
-//    buttons.forEach(button  => {
-//        button.addEventListener("click", () => {
-//            gridSize = button.className;
-//            createGrid(gridSize);
-//        });
-//    });
-//};
