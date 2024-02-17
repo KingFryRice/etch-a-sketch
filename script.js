5109f5f0d2fbe7bf022c39494cef1abaccd9bf68
KingFryRice <graysonbeatonrice@gmail.com>
2024-02-17 12:04:59 -0800

    Get button name to update grid size and change div class name

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const grid = document.querySelector(".container");
 const buttons = document.querySelectorAll("button")
 
 let gridSize = 16;
+let newGridSize;
+let buttonClass;
 
 createGrid(gridSize);
 updateGrid();
@@ -17,14 +19,34 @@ function  createGrid(gridSize) {
      };
 };
 
+function newGrid(newGridSize) {
+    for (let j = 0; j < (newGridSize * newGridSize); j++) {
+        const newCell = document.createElement("div");
+        if (buttonClass === "Small") {
+            newCell.className = "small-cell";
+        } else if (buttonClass === "Medium") {
+            newCell.className = "medium-cell";
+        } else {
+            newCell.className = "large-cell";
+        }
+        grid.appendChild(newCell);
+        newCell.addEventListener ("mouseover", () => {
+            newCell.style = "background: rgb(32, 31, 31); transition: .1s;";
+        });
+    };
+};
+
 function updateGrid () {
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            let newGridSize = button.className;
-            console.log(newGridSize);
             do {
                 grid.removeChild(grid.lastChild);
             } while (grid.childElementCount > 0);
+            newGridSize = button.className;
+            buttonClass = button.textContent;
+            console.log(buttonClass);
+            console.log(newGridSize);
+            newGrid(newGridSize);
         });
     });
 };
